1c7d6a2e284202a5b4471def5584f896d2d8607f
Sreejith K <sreejithemk@gmail.com>
2011-05-13 21:12:18 +0530

    read default values from config

--- pytt/tracker.py
+++ pytt/tracker.py
@@ -14,7 +14,8 @@ import tornado.web
 import tornado.httpserver
 from optparse import OptionParser
 from bencode import bencode, bdecode
-from utils import get_peer_list
+from utils import *
+
 
 class TrackerStats(tornado.web.RequestHandler):
     """Shows the Tracker statistics on this page.
@@ -27,9 +28,12 @@ class AnnounceHandler(tornado.web.RequestHandler):
     """Track the torrents. Respond with the peer-list.
     """
     def get(self):
+        failure_reason = ''
+        warning_message = ''
         # Get all the supported parameters from the HTTP request.
         info_hash = self.get_argument('info_hash')
         peer_id = self.get_argument('peer_id')
+        ip = self.get_argument('ip') or self.request.remote_ip
         port = self.get_argument('port')
         uploaded = self.get_argument('uploaded')
         downloaded = self.get_argument('downloaded')
@@ -37,25 +41,34 @@ class AnnounceHandler(tornado.web.RequestHandler):
         compact = self.get_argument('compact')
         no_peer_id = self.get_argument('no_peer_id')
         event = self.get_argument('event')
-        ip = self.get_argument('ip') or self.request.remote_ip
         numwant = self.get_argument('numwant')
         key = self.get_argument('key')
         trackerid = self.get_argument('trackerid')
 
+        # store the peer info
+        store_peer_info(info_hash, peer_id, ip, port)
+
         # generate response
         response = {}
-        response['failure reason'] = ''
-        response['warning message'] = ''
-        response['interval'] = 5
-        response['min interval'] = 1
+        # Interval in seconds that the client should wait between sending 
+        #    regular requests to the tracker.
+        response['interval'] = get_config().get_int('interval')
+        # Minimum announce interval. If present clients must not reannounce 
+        #    more frequently than this.
+        response['min interval'] = get_config().get_int('min_interval')
         response['tracker id'] = tracker_id
-        response['complete'] = 0
-        response['incomplete'] = 0
+        response['complete'] = get_numof_seeders()
+        response['incomplete'] = get_numof_leechers()
         response['peers'] = get_peer_list()
+        if failure_reason:
+            response['failure reason'] = failure_reason
+        if warning_message:
+            response['warning message'] = warning_message
 
         # send the bencoded response as text/plain document.
         self.set_header('content-type', 'text/plain')
         self.write(bencode(response))
+
 
 class ScrapeHandler(tornado.web.RequestHandler):
     """Returns the state of all torrents this tracker is managing.
@@ -68,7 +81,7 @@ def run_app(port):
     """Start Tornado IOLoop for this application.
     """
     tracker = tornado.web.Application([
-        (r"/announce",AnnounceHandler),
+        (r"/announce", AnnounceHandler),
         (r"/scrape", ScrapeHandler),
         (r"/", TrackerStats),
     ])
@@ -83,26 +96,32 @@ def start_tracker():
     """
     # parse commandline options
     parser = OptionParser()
-    parser.add_option('-p', '--port', help='Tracker Port', default=8080)
+    parser.add_option('-p', '--port', help='Tracker Port', default=0)
     parser.add_option('-b', '--background', action='store_true', 
                     default=False, help='Start in background')
     parser.add_option('-d', '--debug', action='store_true', 
                     default=False, help='Debug mode')
     (options, args) = parser.parse_args()
 
+    # setup directories
+    create_pytt_dirs()
+
     # set debug option
     if options.debug:
         logging.basicConfig(level=logging.DEBUG)
     else:
-    	logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.INFO)
 
     try:
         # start the torrent tracker
-        run_app(int(options.port))
+        run_app(int(options.port) or get_config().get_int('port'))
     except KeyboardInterrupt:
         logging.info('Tracker Stopped.')
+        close_db()
+        sys.exit(0)
     except Exception, ex:
         logging.fatal('%s' %str(ex))
+        close_db()
         sys.exit(-1)
 
 
