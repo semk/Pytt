3f67031f837d7665e36fc6fd9570250fada0c100
Sreejith K <sreejithemk@gmail.com>
2011-05-13 16:11:08 +0530

    common utilities for Pytt

---
+++ pytt/utils.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#
+# Common utilities for Pytt.
+#
+# @author: Sreejith K <sreejithemk@gmail.com>
+# Created on 12th May 2011
+# http://foobarnbaz.com
+
+
+def get_peer_list():
+    """Get all the peer's info with peer_id, ip and port.
+    Eg: [{'peer_id':'#1223&&IJM', 'ip':'162.166.112.2', 'port': '7887'}, ...]
+    """
+    return []
