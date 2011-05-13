5889bbda7beb849f238cd53f496cd392f46316f2
Sreejith K <sreejithemk@gmail.com>
2011-05-13 15:24:05 +0530

    Tornado server setup

--- pytt/tracker.py
+++ pytt/tracker.py
@@ -6,16 +6,22 @@
 # Created on 12th May 2011
 # http://foobarnbaz.com
 
+
+import sys
 import logging
 import tornado.ioloop
 import tornado.web
+import tornado.httpserver
+from optparse import OptionParser
 from bencode import bencode, bdecode
+
 
 class TrackerStats(tornado.web.RequestHandler):
     """Shows the Tracker statistics on this page.
     """
     def get(self):
         self.send_error(404)
+
 
 class AnnounceHandler(tornado.web.RequestHandler):
     """Track the torrents. Respond with the peer-list.
@@ -36,18 +42,55 @@ class AnnounceHandler(tornado.web.RequestHandler):
         key = self.get_argument('key')
         trackerid = self.get_argument('trackerid')
 
+
 class ScrapeHandler(tornado.web.RequestHandler):
     """Returns the state of all torrents this tracker is managing.
     """
     def get(self):
         self.send_error(404)
 
-tracker = tornado.web.Application([
-    (r"/announce",AnnounceHandler),
-    (r"/scrape", ScrapeHandler),
-    (r"/", TrackerStats),
-])
 
-if __name__ == "__main__":
-    tracker.listen(8888)
+def run_app(port):
+    """Start Tornado IOLoop for this application.
+    """
+    tracker = tornado.web.Application([
+        (r"/announce",AnnounceHandler),
+        (r"/scrape", ScrapeHandler),
+        (r"/", TrackerStats),
+    ])
+    logging.info('Starting Pytt on port %d' %port)
+    http_server = tornado.httpserver.HTTPServer(tracker)
+    http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
+
+
+def start_tracker():
+    """Start the Torrent Tracker.
+    """
+    # parse commandline options
+    parser = OptionParser()
+    parser.add_option('-p', '--port', help='Tracker Port', default=8080)
+    parser.add_option('-b', '--background', action='store_true', 
+                    default=False, help='Start in background')
+    parser.add_option('-d', '--debug', action='store_true', 
+                    default=False, help='Debug mode')
+    (options, args) = parser.parse_args()
+
+    # set debug option
+    if options.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+    	logging.basicConfig(level=logging.INFO)
+
+    try:
+        # start the torrent tracker
+        run_app(int(options.port))
+    except KeyboardInterrupt:
+        logging.info('Tracker Stopped.')
+    except Exception, ex:
+        logging.fatal('%s' %str(ex))
+        sys.exit(-1)
+
+
+if __name__ == '__main__':
+    start_tracker()
