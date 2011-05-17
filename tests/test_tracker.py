4ed664b6c8dd10353c6fd7cc3a18587a3702620d
Sreejith K <sreejithemk@gmail.com>
2011-05-17 19:01:32 +0530

    Tests updated to use AsyncHTTPTestCase

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Common utilities for Pytt.
+# TestCases for Pytt Tracker 
 #
 # @author: Sreejith K <sreejithemk@gmail.com>
 # Created on 12th May 2011
@@ -10,41 +10,81 @@
 import os
 import sys
 from nose import with_setup
-import subprocess
 import urllib2
 import urllib
 import hashlib
+from tornado.testing import AsyncHTTPTestCase
 
 sys.path.append('../')
-from pytt.bencode import bencode
+from pytt.tracker import *
 
 
-def start_tracker():
-    tracker = os.path.abspath('../pytt/tracker.py')
-    subprocess.Popen(['python', tracker])
+# define application
+app = tornado.web.Application([
+            (r"/announce.*", AnnounceHandler),
+            (r"/scrape.*", ScrapeHandler),
+            (r"/", TrackerStats),
+        ])
 
 
-def stop_tracker():
-    raise KeyboardInterrupt
+def clear_db():
+    """Clear the tracker db.
+    """
+    try:
+        os.remove(DB_PATH)
+    except:
+        pass
 
 
-@with_setup(start_tracker, stop_tracker)
-def test_tracker():
-    pass
+class TestHandlerBase(AsyncHTTPTestCase):
+    """Base Test class for all request handlers.
+    """
+    def setUp(self):
+        """Do this before every test
+        """
+        clear_db()
+        super(TestHandlerBase, self).setUp()
+
+    def get_app(self):
+        """Get the application object
+        """
+        return app
+
+    def get_http_port(self):
+        """Set Tracker listen port from config or default 8080
+        """
+        return get_config().getint('tracker', 'port') or 8080
 
 
-if __name__ == '__main__':
-    info = {'piece length': 1024,
-            'pieces': hashlib.sha1('crap').hexdigest(),
-            'private': 0
-            }
-    bencoded_info = bencode(info)
-    info_hash = hashlib.sha1(bencoded_info).hexdigest()
-    query = {'info_hash': info_hash,
-             'peer_id': 'BitTorrent-1.0',
-             'ip': '112.113.144.1',
-             'port': '6881'
-             }
-    query = urllib.urlencode(query)
-    res = urllib2.urlopen('http://127.0.0.1:8080/announce?%s' %query)
-    print res
+class TestAnnounceHandler(TestHandlerBase):
+    """Test cases for Announce request for the Torrent Tracker
+    """
+    def announce_test(self):
+        """Test response for Announce.
+        """
+        # torrent meta info
+        info = {'piece length': 1024,
+                'pieces': hashlib.sha1('crap').digest(),
+                'private': 0
+                }
+        # bencode meta info
+        bencoded_info = bencode(info)
+        # take SHA-1 hash of this value
+        info_hash = hashlib.sha1(bencoded_info).digest()
+        # check info_hash_length
+        self.assertEqual(len(info_hash), INFO_HASH_LEN)
+        # make an announce query
+        query = {'info_hash': info_hash,
+                 'peer_id': 'BitTorrent-1.0',
+                 'ip': '112.113.144.1',
+                 'port': '6881'
+                 }
+        # urlencode this dictionary
+        query = urllib.urlencode(query)
+        # send GET request to /announce
+        response = self.fetch('/announce', 
+                            method='GET', 
+                            body=query, 
+                            follow_redirects=False)
+        # if successful, should return 200-OK 
+        self.assertEqual(response.code, 200)
